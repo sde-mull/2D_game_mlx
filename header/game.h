88d9ad7ab48f84ad912ca790f868eaaca4e60e36
kuhaku <sebastiao.muller@gmail.com>
2023-04-25 22:27:25 +0100

    organizing files

--- header/game.h
+++ header/game.h
@@ -6,7 +6,7 @@
 /*   By: sde-mull <sde-mull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 17:39:16 by sde-mull          #+#    #+#             */
-/*   Updated: 2023/04/25 22:14:51 by sde-mull         ###   ########.fr       */
+/*   Updated: 2023/04/25 22:26:48 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@
 # include "macros.h"
 
 //Window.c
-int	    exit_game(void);
 bool	init_window(t_win *win);
 void    window_management(void);
 
 //rendering.c
-int render(void);
+int     render(void);
+void    get_fps();
 
 //key.c
 int scan_key(int keycode);
@@ -52,6 +52,9 @@ t_img	load_images(t_win *win, char *str);
 t_img	new_image(t_win *win, int x, int y);
 void	init_images(t_win *win);
 
+//free.c
 void	free_array(char **arr);
+void    free_win(t_win *win);
+int	    exit_game(void);
 
 #endif
